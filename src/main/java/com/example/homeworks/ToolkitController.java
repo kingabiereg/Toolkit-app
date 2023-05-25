c001e9a1e92e3a2d95170dfee05b382ebdf1abbb
Kinga Biereg <stachewicz.k@gmail.com>
2023-05-25 19:18:28 +0200

    Added methods to ToolkitController

--- src/main/java/com/example/homeworks/ToolkitController.java
+++ src/main/java/com/example/homeworks/ToolkitController.java
@@ -4,7 +4,9 @@ package com.example.homeworks;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -24,6 +26,20 @@ public class ToolkitController {
         return "tools";
     }
 
+@GetMapping("add")
+    public String addTool() {
+
+        return "add";
+    }
+
+    @PostMapping("save")
+    public String addTool(@RequestParam String name, @RequestParam float size, @RequestParam String unit, Model model) {
+        Tool addTool = new Tool(name, new Tool.ToolSize(size, unit));
+        toolkitRepository.add(addTool);
+        List<Tool> tools = toolkitRepository.getTools();
+        model.addAttribute("tools", tools);
+        return "save";
+    }
 }
 
 
